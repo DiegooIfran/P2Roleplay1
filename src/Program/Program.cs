829b0962e9086011efbfeb504c87e25fae7aeabc
Diego <diego.ifran@correo.ucu.edu.uy>
2025-09-22 08:55:05 -0300

    main

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,1 +1,37 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Library;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Selecciona un tipo de personaje (elfo, enano, mago):");
+        string pj = Console.ReadLine();
+        switch (pj)
+        {
+            case "elfo":
+                Console.WriteLine("Elegiste ser elfo");
+                Console.WriteLine("Selecciona un nombre:");
+                string name = Console.ReadLine();
+                Elves elfo = new Elves(name, 100);
+                Console.WriteLine($"Eres {name}, un elfo.");
+                break;
+            case "enano":
+                Console.WriteLine("Elegiste ser enano");
+                Console.WriteLine("Selecciona un nombre:");
+                name = Console.ReadLine();
+                Dwarves enano = new Dwarves(name, 120);
+                Console.WriteLine($"Eres {name}, un enano.");
+                break;
+            case "mago":
+                Console.WriteLine("Elegiste ser mago");
+                Console.WriteLine("Selecciona un nombre:");
+                name = Console.ReadLine();
+                Wizards mago = new Wizards(name, 80);
+                Console.WriteLine($"Eres {name}, un mago.");
+                break;
+            default:
+                Console.WriteLine("No elegiste un personaje válido");
+                break;
+        }
+    }
+}
