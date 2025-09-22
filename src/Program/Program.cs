579d5d8b05b754c38b7974a0ca0e6fa6207ce5da
Diego <diego.ifran@correo.ucu.edu.uy>
2025-09-22 08:58:28 -0300

    main

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -4,30 +4,30 @@ internal class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Selecciona un tipo de personaje (elfo, enano, mago):");
+        Console.Write("Selecciona un tipo de personaje (elfo, enano, mago): ");
         string pj = Console.ReadLine();
         switch (pj)
         {
             case "elfo":
-                Console.WriteLine("Elegiste ser elfo");
-                Console.WriteLine("Selecciona un nombre:");
+                Console.WriteLine($"Elegiste ser {pj}");
+                Console.Write("Selecciona un nombre: ");
                 string name = Console.ReadLine();
                 Elves elfo = new Elves(name, 100);
-                Console.WriteLine($"Eres {name}, un elfo.");
+                Console.WriteLine($"Eres {name}, un {pj}.");
                 break;
             case "enano":
-                Console.WriteLine("Elegiste ser enano");
-                Console.WriteLine("Selecciona un nombre:");
+                Console.WriteLine($"Elegiste ser {pj}");
+                Console.Write("Selecciona un nombre: ");
                 name = Console.ReadLine();
                 Dwarves enano = new Dwarves(name, 120);
-                Console.WriteLine($"Eres {name}, un enano.");
+                Console.WriteLine($"Eres {name}, un {pj}.");
                 break;
             case "mago":
-                Console.WriteLine("Elegiste ser mago");
-                Console.WriteLine("Selecciona un nombre:");
+                Console.WriteLine($"Elegiste ser {pj}");
+                Console.Write("Selecciona un nombre: ");
                 name = Console.ReadLine();
                 Wizards mago = new Wizards(name, 80);
-                Console.WriteLine($"Eres {name}, un mago.");
+                Console.WriteLine($"Eres {name}, un {pj}.");
                 break;
             default:
                 Console.WriteLine("No elegiste un personaje válido");
