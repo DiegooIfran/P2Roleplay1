7edf8ca833237bbb31b0153e3fd6436423ae9cda
Luutyy <lautaramirezlopez@gmail.com>
2025-09-21 21:25:24 -0300

    Testeo Dwarves

--- src/Library/Item.cs
+++ src/Library/Item.cs
@@ -2,10 +2,14 @@
 
 public class Item
 {
+    // Aplicamos el principio de encapsulamiento: propiedades con "private set",
+    // para que solo puedan ser modificadas en el constructor
     public int Defense { get; private set; }
     public int Attack { get; private set; }
     public string Type { get; private set; }
-
+    
+    
+    // Esto garantiza que todo Item creado tenga un estado válido desde el inicio
     public Item(int defense, int attack, string type)
     {
         Defense = defense;
