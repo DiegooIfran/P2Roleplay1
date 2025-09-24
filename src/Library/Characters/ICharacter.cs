13405ebb1f9a1c97531591dfad4fddbdf6cb0fc9
Luutyy <lautaramirezlopez@gmail.com>
2025-09-24 10:10:44 -0300

    Implementacion de ICharacter

---
+++ src/Library/Characters/ICharacter.cs
@@ -0,0 +1,17 @@
+namespace Library;
+
+public interface ICharacter
+{
+    string Name { get; }  
+    IReadOnlyList<Item> Items { get; } 
+    int Health { get; set; }
+    int MaxHealth { get; }
+
+    void AddItem(IItem item);
+    void RemoveItem(IItem item);
+    int GetAttack();
+    int GetDefense();
+    int GetHeal();
+    void Heal();
+    void Attack(ICharacter target);
+}
