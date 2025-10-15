33c8e81ba45b9ec15d76a09b0d388b23840ed066
Diego <diego.ifran@correo.ucu.edu.uy>
2025-10-15 10:35:06 -0300

    elfo nuevo

--- src/Library/Characters/Heroes/Elf.cs
+++ src/Library/Characters/Heroes/Elf.cs
@@ -1,82 +1,14 @@
+using Library.Characters.Heroes;
+
 namespace Library;
 
 // Creamos esta clase separada porque cada tipo de personaje
 // puede tener comportamientos únicos en el futuro
 // De esta manera, aplicamos el Principio de Responsabilidad Única (SRP)
 // y el código queda preparado para extenderse sin romper a los demás personajes
-public class Elf : ICharacter
+public class Elf : Hero
 {
-    public string Name { get; set; }
-    private List<IItem> _items = new List<IItem>();
-    public int Health { get; set; }
-    public int BaseHealth { get; set; }
-
-    public Elf(string name, int health) //Constructor de elfo
+    public Elf(string name, int health) : base(name,health)
     {
-        Name = name;
-        BaseHealth = health; //La variable MaxHealth se usara para restaurar la vida en otro momento
-        Health = health;
-    }
-
-    public int GetAttack()
-    {
-        int attack = 0;
-        foreach (IItem objeto in _items)
-        {
-            if (objeto is IAttackItem attackItem)
-            {
-                attack += attackItem.Attack;
-            }
-        }
-        // Recorre los items que tiene el personaje sumando los daños de cada uno
-        return attack;
-    }
-    
-    public int GetDefense()
-    {
-        int defense = 0;
-        foreach (IItem objeto in _items)
-        {
-            if (objeto is IDefenseItem defenseItem)
-            {
-                defense += defenseItem.Defense;
-            }
-        }
-        // Recorre los items que tiene el personaje sumando las defensas de cada uno
-        
-        return defense;
-    }
-    
-    public List<IItem> GetItems()
-    {
-        return _items;
-    }
-    
-    public int GetHealth() //Devuelve la vida actual
-    {
-        return Health;
-    }
-    
-    public void Heal() //Cura al maximo
-    {
-        Health = BaseHealth;
-    }
-    
-    public void AddItem(IItem item) //Añade un item
-    {
-        _items.Add(item);
-    }
-
-    public void RemoveItem(IItem item) //Quita un item
-    {
-        _items.Remove(item);
-    }
-    
-    public void Attack(ICharacter target) //Ataca y le quita vida segun la defensa del objetivo y el ataque del elfo
-    {
-        if ((this.GetAttack() - target.GetDefense()) >= 0) //Para checkear que el daño sea un numero positivo
-        {
-            target.Health = target.Health - (this.GetAttack() - target.GetDefense());
-        }
     }
 }
