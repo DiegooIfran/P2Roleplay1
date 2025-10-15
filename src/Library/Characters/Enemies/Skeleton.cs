296fe939959e6cea1f2920cc238450c75a832a3f
Luutyy <lautaramirezlopez@gmail.com>
2025-10-15 10:21:16 -0300

    Skeleton

--- src/Library/Characters/Enemies/Skeleton.cs
+++ src/Library/Characters/Enemies/Skeleton.cs
@@ -2,8 +2,6 @@ namespace Library.Characters.Enemies;
 
 public class Skeleton: Enemy
 {
-    private string _name;
-    public int Health;
     public Skeleton(string name, int health, int vp) : base(name, health, vp)
     { }
 }
