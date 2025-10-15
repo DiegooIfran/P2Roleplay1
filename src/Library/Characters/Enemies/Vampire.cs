de7c72435f57d933efcf7bcf611321b1f7afca7a
paulinasilveira <paulina.silveira@correo.ucu.edu.uy>
2025-10-15 10:17:15 -0300

    Arreglo enemy y vampire

--- src/Library/Characters/Enemies/Vampire.cs
+++ src/Library/Characters/Enemies/Vampire.cs
@@ -1,13 +1,16 @@
 namespace Library.Characters;
 
-public class Vampire
+public class Vampire : Enemy
 {
-    public string Name { get; set; }
-    public int Health { get; set; }
-
-    public Vampire(string name, int health) //Constructor de vampiro
+    public Vampire(string name, int health) : base(name, health)
     {
         Name = name;
         Health = health;
     }
+
+    public string Name { get; set; }
+    public int Health { get; set; }
+
+  
 }
+
