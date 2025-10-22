1d359b08b9ce6b20fae824713ce773b60e85ce68
Luutyy <lautaramirezlopez@gmail.com>
2025-10-22 10:37:38 -0300

    Arreglo testeos elf y dwarf

--- test/LibraryTests/TestsHero/TestElf.cs
+++ test/LibraryTests/TestsHero/TestElf.cs
@@ -1,5 +1,6 @@
 using Library;
 using Library.Characters;
+using Library.Characters.Enemies;
 using Ucu.Poo.RoleplayGame;
 
 namespace LibraryTests;
@@ -42,56 +43,29 @@ public class TestElf
     }
     
     [Test]
-    public void TestAttackElf()
+    public void TestAttack()
     {
         //Testeo el metodo para atacar a otro elfo
         Elf elfAtt = new Elf("Legolas", 100);
         Bow itemAtt = new Bow("Bow");
         Shield itemDef = new Shield("Shield");
         elfAtt.AddItem(itemAtt);
-        elfAtt.AddItem(itemDef);
-        Elf elfDef = new Elf("Galadriel", 100);
-        elfAtt.Attack(elfDef);
-        Assert.That(elfDef.Health, Is.Not.EqualTo(100));
+        elfAtt.AddItem(itemDef);;
+        Demon demonDef = new Demon("Carlos", 100, 10);
+        elfAtt.Attack(demonDef);
+        Assert.That(demonDef.Health, Is.Not.EqualTo(100));
     }
     
-    [Test]
-    public void TestAttackDwarf()
-    {
-        //Testeo el metodo para atacar a los enanos
-        Elf elfAtt = new Elf("Legolas", 100);
-        Bow itemAtt = new Bow("Bow");
-        Shield itemDef = new Shield("Shield");
-        elfAtt.AddItem(itemAtt);
-        elfAtt.AddItem(itemDef);
-        Dwarf dwarf = new Dwarf("Durin", 120);
-        elfAtt.Attack(dwarf);
-        Assert.That(dwarf.Health, Is.Not.EqualTo(120));
-    }
-    
-    [Test]
-    public void TestAttackWizard()
-    {
-        //Testeo el metodo para atacar a los magos
-        Elf elfAtt = new Elf("Legolas", 100);
-        Bow itemAtt = new Bow("Bow");
-        Shield itemDef = new Shield("Shield");
-        elfAtt.AddItem(itemAtt);
-        elfAtt.AddItem(itemDef);
-        Wizard wizard = new Wizard("Gandalf", 80);
-        elfAtt.Attack(wizard);
-        Assert.That(wizard.Health, Is.Not.EqualTo(80));
-    }
     
     [Test]
     public void Heal()
     {
         //Testeo el recibir el ataque y la funcion Heal, la cual deberia restaurar la vida.
         Elf elf1 = new Elf("Legolas", 100);
-        Elf elf2 = new Elf("Galadriel", 100);
         Bow itemAtt = new Bow("Bow");
-        elf2.AddItem(itemAtt);
-        elf2.Attack(elf1);
+        Demon demonAttack = new Demon("Carlos", 100, 10);
+        demonAttack.AddItem(itemAtt);
+        demonAttack.Attack(elf1);
         Assert.That(elf1.GetHealth(), Is.EqualTo(90));
         elf1.Heal();
         Assert.That(elf1.GetHealth(), Is.EqualTo(100));
